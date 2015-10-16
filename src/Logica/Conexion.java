bc1c3dc7f2e48345db5e4a7fc26126b9e4028b14
Luis Torres Patiño <Luis Torres Patio@LuisTorresP.connectify>
2015-10-16 16:47:39 -0500

    hi

---
+++ src/Logica/Conexion.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Luis Torres Patiño
+ */
+public class Conexion {
+    public String db="BaseReserva";
+    public String url="jdbc:mysql://127.0.0.1" + db; 
+    public String user="root";
+    public String pass="";
+
+    public Conexion() {
+        
+    }
+      public Connection conectar(){
+          Connection link=null;
+        
+          try {
+              Class.forName("org.git.mm.mysql.Driver"); 
+              link=DriverManager.getConnection(this.url, this.user, this.pass);       
+          } catch (ClassNotFoundException | SQLException e) {
+              JOptionPane.showConfirmDialog(null, e);
+          }
+        return link;
+        
+      }
+    
+}
